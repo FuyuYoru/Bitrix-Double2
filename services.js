a3901c9c392c059cd49750420fd3f376a45c2dd1
FuyuYoru <vvelichko.2000@mail.ru>
2023-12-25 17:27:52 +0500

    changes

--- services.js
+++ services.js
@@ -1,9 +1,11 @@
 const xhr = new XMLHttpRequest()
-
+console.log('грузимся');
 xhr.open('GET', 'http://192.168.91.89/GetUpdStatus', true)
+console.log('грузимся');
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200){
         const xmlDoc = xhr.responseXML;
         console.log(xmlDoc)
     }
 }
+xhr.send();
